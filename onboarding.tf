7147d87a1d84a60ff7e2272e84832d8386aeb040
Jimmy Yeung <94708005+hashinyc@users.noreply.github.com>
2024-07-05 17:00:07 -0400

    Update onboarding.tf

--- onboarding.tf
+++ onboarding.tf
@@ -8,21 +8,21 @@ module "strongdm_onboarding" {
   # EKS resources take approximately 20 min
   # NOTE: Before creating AKS resources, set up Service Principal by following these instructions https://docs.microsoft.com/en-us/azure/aks/kubernetes-service-principal?tabs=azure-cli
   # create_aks               = true, set up a service
-  # Mysql resources take approximately 5 min
-  # create_mysql             = true
-  # RDP resources take approximately 10 min
-  # create_rdp               = true
-  # HTTP resources take approximately 5 min
+  Mysql resources take approximately 5 min
+  create_mysql             = true
+  RDP resources take approximately 10 min
+  create_rdp               = true
+  HTTP resources take approximately 5 min
   # NOTE: Before creating HTTP resources, set up TLS here https://app.strongdm.com/app/datasources/websites
   # create_http              = false
-  # SSH resources take approximately 5 min
-  # create_ssh              = true
-  # Gateways take approximately 5 min
-  # create_strongdm_gateways = true
+  SSH resources take approximately 5 min
+  create_ssh              = true
+  Gateways take approximately 5 min
+  create_strongdm_gateways = true
 
-  # VPC creation takes approximately 5 min
-  # If set to false the default VPC will be used instead
-  # create_resource_group = true
+  #VPC creation takes approximately 5 min
+  I#f set to false the default VPC will be used instead
+  create_resource_group = true
 
   # Tags will be added to strongDM and AWS resources.
   # tags = {}
